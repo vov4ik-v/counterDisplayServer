0397273df6d874b858c0041f9d939c1d9940428a
vov4ik-v <83838066+vov4ik-v@users.noreply.github.com>
2024-03-21 15:51:35 +0200

    Delete doesn`t relevant text and fix space in create_device_table.sql

--- src/main/resources/db/changelog/scripts/create_device_table.sql
+++ src/main/resources/db/changelog/scripts/create_device_table.sql
@@ -1,14 +1,14 @@
 CREATE TABLE if not exists device
 (
     number_of_device BIGINT AUTO_INCREMENT PRIMARY KEY,
-    cantora_name    VARCHAR(255) default null,
-    counter_type    INT  default null,
-    address        VARCHAR(255)  default null,
-    frequency      BIGINT  default null,
-    password       VARCHAR(255)  default null,
-    price          INT  default null,
-    user_id        BIGINT  default null,
-    created_date    VARCHAR(255) NOT NULL,
-    FOREIGN KEY fk_user_id (user_id) REFERENCES user(id),
+    cantora_name VARCHAR(255) default null,
+    counter_type INT default null,
+    address VARCHAR(255) default null,
+    frequency BIGINT default null,
+    password VARCHAR(255) default null,
+    price INT default null,
+    user_id BIGINT default null,
+    created_date VARCHAR(255) NOT NULL,
+    FOREIGN KEY fk_user_id (user_id) REFERENCES user (id),
     INDEX idx_user_id (user_id)
 );
