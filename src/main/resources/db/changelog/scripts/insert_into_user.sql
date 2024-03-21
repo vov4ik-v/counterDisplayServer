ca106cc81fa0b3ce9237432bdb56717bebf573ef
vov4ik-v <83838066+vov4ik-v@users.noreply.github.com>
2024-03-21 18:08:36 +0200

    Feature/add-script-to-dro-table-and-insert-user (#3)
    * Add liquibase(create tables and insert) and fix bug with docker and liquibase
    
    * Delete doesn`t relevant text and fix space in create_device_table.sql
    
    * Add script to drop the tables

--- src/main/resources/db/changelog/scripts/insert_into_user.sql
+++ src/main/resources/db/changelog/scripts/insert_into_user.sql
@@ -4,3 +4,4 @@ VALUES
     ('Lviv','28','Lviv','Green',null,CURRENT_TIMESTAMP,'test2@gmail.com','test2','test2','$2a$10$yppTWyl.p7zY3W3rf0c5YuzO2j4BwvLYNGm8J7GUHn2RaZcabsDXG','+380991262441','test2'),
     ('Lviv','16','Lviv','Mazepi',null,CURRENT_TIMESTAMP,'test3@gmail.com','test3','test3','$10$VhTSEIMey3vMvy7HEAjHdeDLmyo8x8CVQirDUMFSnL9R4FPVf4lgW','+380991262441','test3'),
     ('Lviv','2','Lviv','Bandera',null,CURRENT_TIMESTAMP,'test4@gmail.com','test4','test4','$2a$10$a5CKa9Mi0AWk.4H5Xk5F9ugec.gl/4PrXLuHgHIRIGOpptDXk9VJC','+380991262441','test4')
+    
