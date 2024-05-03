056f1f771125363f4243eade182f8333cd780a96
vov4ik-v <83838066+vov4ik-v@users.noreply.github.com>
2024-05-04 00:18:10 +0300

    Update src/main/java/com/spm/vasylyshyn/exeptions/UsernameAlreadyExistException.java
    Co-authored-by: Богдан Дідух <74701445+BogdanDidukh2003@users.noreply.github.com>

--- src/main/java/com/spm/vasylyshyn/exeptions/UsernameAlreadyExistException.java
+++ src/main/java/com/spm/vasylyshyn/exeptions/UsernameAlreadyExistException.java
@@ -8,7 +8,5 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 public class UsernameAlreadyExistException extends RuntimeException {
     public UsernameAlreadyExistException(String message){
         super(message);
-
-
     }
 }
